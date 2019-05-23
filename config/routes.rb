87df683833fca96605056c896ff73469f9f5110e
Ethan Grab <ethangrab@gmail.com>
2019-05-23 01:03:51 -0600

    Routes to begin implementation of oauth

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root 'welcome#index'
+
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  get '/register', to: 'users#new'
+  get '/auth/github/callback', to: 'users#update'
+
 end
