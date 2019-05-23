134a0b133cc4f941f0e7564077a9f813a7b26c04
Chi Tran <chidtran@gmail.com>
2019-05-22 20:21:24 -0600

    Changes paid to validates_inclusion_of

--- app/models/user_activity.rb
+++ app/models/user_activity.rb
@@ -2,7 +2,7 @@ class UserActivity < ApplicationRecord
   belongs_to :user
   belongs_to :activity
 
-  validates_presence_of :paid
+  validates_inclusion_of :paid, in: [:true, :false]
   validates :price, presence: true, numericality: { greater_than: 0 }
   validates :quantity, presence: true, numericality: { greater_than: -1 }
 end 
--- spec/models/user_activity_spec.rb
+++ spec/models/user_activity_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe UserActivity do
   describe 'validations' do
-    it { should validate_presence_of :paid }
+    it { should validate_inclusion_of(:paid).in_array([true, false])}
     it { should validate_numericality_of(:price).is_greater_than(0) }
     it { should validate_numericality_of(:quantity).is_greater_than(-1) }
   end
