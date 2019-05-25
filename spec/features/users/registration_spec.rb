6fabc7bd384920b38564d1cfacec6ff120bb2cf8
Jalena Taylor <45905026+jalena-penaligon@users.noreply.github.com>
2019-05-25 11:23:57 -0600

    Creates test to verify that an email must be unique

--- spec/features/users/registration_spec.rb
+++ spec/features/users/registration_spec.rb
@@ -24,5 +24,22 @@ describe 'as a visitor' do
       click_link("Do This Later")
       expect(current_path).to eq(dashboard_path)
     end
+
+    it 'can will not allow me to register with an email address that exists' do
+      user = User.create(first_name: "First", last_name: "Last", email: "email@email.com", password:"password")
+      visit '/'
+      click_link "Register"
+      expect(current_path).to eq(new_user_path)
+      expect(page).to have_content("Create an Account")
+
+      fill_in "user[first_name]", with: "First"
+      fill_in "user[last_name]", with: "Last"
+      fill_in "user[email]", with: "email@email.com"
+      fill_in "user[password]", with: "password"
+      fill_in "user[confirm_password]", with: "password"
+      click_button "Register"
+
+      expect(page).to have_content("Email has already been taken")
+    end
   end
 end
