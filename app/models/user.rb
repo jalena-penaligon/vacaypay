94004790ebd8da4a109c8cc1c79c0d984ed49ae5
Jalena Taylor <45905026+jalena-penaligon@users.noreply.github.com>
2019-05-25 11:26:12 -0600

    Adds test that a user cannot be created with missing info

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
   has_many :vacations, through: :vacation_users
 
   validates_uniqueness_of :email
-  validates_presence_of :first_name, :last_name
+  validates_presence_of :first_name, :last_name, :email 
 
   validates_confirmation_of :password
   has_secure_password
