8c94b6537b5156850d3e14175141bc8df40144dd
Ethan Grab <ethangrab@gmail.com>
2019-05-25 14:24:14 -0600

    Route for dwolla access token

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root to: "welcome#index"
 
   get '/dashboard', to: 'users/vacations#index', as: :dashboard
+  get '/oauth', to: 'users/users#create', as: :users_auth
 end
