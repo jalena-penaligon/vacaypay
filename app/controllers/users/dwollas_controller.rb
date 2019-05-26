cc6bf4a5dad378b23d4ff0f0dda2031613594d75
Ethan Grab <ethangrab@gmail.com>
2019-05-26 16:19:16 -0600

    create restful method to grab client token and create customer on dwolla
    Co-authored-by: Earl Stephens <34906415+earl-stephens@users.noreply.github.com>

--- app/controllers/users/dwollas_controller.rb
+++ app/controllers/users/dwollas_controller.rb
@@ -1,17 +1,35 @@
 class Users::DwollasController < ApplicationController
 
+  def new
+  end
+
   def create
-    token = dwolla_token[:access_token]
+    token = dwolla_token
+    response = dwolla_customer_location(token)
+    current_user.update(dwolla_id: response.response_headers[:location])
+    redirect_to dashboard_path
   end
 
   private
 
-  def dwolla_token
-    @_dwolla_token ||= service.application_token
+  def dwolla_customer_location(token)
+    @_customer_location ||= dwolla_customer_service(token).create_verified_user
   end
 
-  def service
-    @_service ||= DwollaOauthService.new
+  def dwolla_token
+    @_dwolla_token ||= dwolla_oauth_service.application_token
+  end
+
+  def dwolla_oauth_service
+    @_oauth_service ||= DwollaOauthService.new
+  end
+
+  def dwolla_customer_service(token)
+    @_customer_service ||= DwollaCustomerService.new(current_user, user_params, token)
+  end
+
+  def user_params
+    params.permit(:address, :city, :state, :postal_code, :dob, :ssn)
   end
 
 end
