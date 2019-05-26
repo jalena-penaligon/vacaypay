7768cea36263d95eacc65babf521094384625f73
Ethan Grab <ethangrab@gmail.com>
2019-05-25 21:12:06 -0600

    Deleting whitespace

--- app/controllers/users/dwollas_controller.rb
+++ app/controllers/users/dwollas_controller.rb
@@ -1,16 +1,10 @@
-
 class Users::DwollasController < ApplicationController
-
 
   def create
     token = dwolla_token[:access_token]
   end
 
-
-
   private
-
-
 
   def dwolla_token
     @_dwolla_token ||= service.application_token
@@ -19,6 +13,5 @@ class Users::DwollasController < ApplicationController
   def service
     @_service ||= DwollaOauthService.new
   end
-
 
 end
