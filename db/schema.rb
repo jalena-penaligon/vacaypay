44c81154faed74d58df3ec28c26d31122342d059
Ethan Grab <ethangrab@gmail.com>
2019-05-27 09:21:22 -0600

    schema and seed changes to reflect new sti config

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_25_160659) do
+ActiveRecord::Schema.define(version: 2019_05_27_151314) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "activities", force: :cascade do |t|
     t.decimal "price"
-    t.integer "num_attendees", default: 0
+    t.integer "num_attendees", default: 1
     t.text "description"
     t.string "name"
     t.datetime "created_at", null: false
--- db/seeds.rb
+++ db/seeds.rb
@@ -13,13 +13,13 @@ bachelorette = Vacation.create!(name: "bachelorette", location: "Hawaii", start_
   bachelorette_user_1 = bachelorette.vacation_users.create!(user_id: jalena.id, role: 1)
   bachelorette_user_2 = bachelorette.vacation_users.create!(user_id: chi.id, role: 0)
 
-  hawaii_hotel = bachelorette.activities.create!(price: 600.00, num_attendees: 2, description: "Double Room", name: "Hawaii Hotel", no_of_days: 3, cutoff_date: "2019-06-10", type: 1, user_id: jalena.id)
+  hawaii_hotel = bachelorette.activities.create!(price: 600.00, num_attendees: 2, description: "Double Room", name: "Hawaii Hotel", no_of_days: 3, cutoff_date: "2019-06-10", type: FixedCost  , user_id: jalena.id)
 
     jalena_hawaii_hotel = UserActivity.create!(user_id: jalena.id, quantity: 1, price: 300.00, paid: false, activity_id: hawaii_hotel.id)
 
     chi_hawaii_hotel = UserActivity.create!(user_id: chi.id, quantity: 1, price: 300.00, paid: false, activity_id: hawaii_hotel.id)
 
-  snorkeling = bachelorette.activities.create!(price: 200.00, num_attendees: 2, description: "Swim with the fishes", name: "Snorkeling", no_of_days: 1, cutoff_date: "2019-06-12", type: 2, user_id: jalena.id)
+  snorkeling = bachelorette.activities.create!(price: 200.00, num_attendees: 2, description: "Swim with the fishes", name: "Snorkeling", no_of_days: 1, cutoff_date: "2019-06-12", type: PerPersonCost, user_id: jalena.id)
 
     jalena_snorkeling = UserActivity.create!(user_id: jalena.id, quantity: 1, price: 100.00, paid: false, activity_id: snorkeling.id)
 
@@ -30,13 +30,13 @@ party = Vacation.create!(name: "Party", location: "Brazil", start_date: "2019-07
   party_user_1 = party.vacation_users.create!(user_id: earl.id, role: 1)
   party_user_2 = party.vacation_users.create!(user_id: ethan.id, role: 0)
 
-  brazil_airbnb = party.activities.create!(price: 800.00, num_attendees: 2, description: "Condo", name: "Brazil AirBnB", no_of_days: 5, cutoff_date: "2019-07-10", type: 1, user_id: earl.id)
+  brazil_airbnb = party.activities.create!(price: 800.00, num_attendees: 2, description: "Condo", name: "Brazil AirBnB", no_of_days: 5, cutoff_date: "2019-07-10", type: FixedCost, user_id: earl.id)
 
       earl_brazil_airbnb = UserActivity.create!(user_id: earl.id, quantity: 1, price: 400.00, paid: false, activity_id: brazil_airbnb.id)
 
       ethan_brazil_airbnb = UserActivity.create!(user_id: ethan.id, quantity: 1, price: 400.00, paid: false, activity_id: brazil_airbnb.id)
 
-    boat_rental = party.activities.create!(price: 400.00, num_attendees: 2, description: "For Friday & Saturday", name: "Boat Rental", no_of_days: 2, cutoff_date: "2019-07-14", type: 2, user_id: earl.id)
+    boat_rental = party.activities.create!(price: 400.00, num_attendees: 2, description: "For Friday & Saturday", name: "Boat Rental", no_of_days: 2, cutoff_date: "2019-07-14", type: PerPersonCost, user_id: earl.id)
 
       earl_boat_rental = UserActivity.create!(user_id: earl.id, quantity: 1, price: 200.00, paid: false, activity_id: boat_rental.id)
 
@@ -48,7 +48,7 @@ wine_tasting = Vacation.create!(name: "Wine Tasting", location: "France", start_
   wt_user_2 = wine_tasting.vacation_users.create!(user_id: earl.id, role: 0)
   wt_user_3 = wine_tasting.vacation_users.create!(user_id: jalena.id, role: 0)
 
-  france_hotel = wine_tasting.activities.create!(price: 1_200.00, num_attendees: 3, description: "3 Rooms", name: "France Hotel", no_of_days: 4, cutoff_date: "2019-08-19", type: 1, user_id: ethan.id)
+  france_hotel = wine_tasting.activities.create!(price: 1_200.00, num_attendees: 3, description: "3 Rooms", name: "France Hotel", no_of_days: 4, cutoff_date: "2019-08-19", type: FixedCost, user_id: ethan.id)
 
       ethan_france_hotel = UserActivity.create!(user_id: ethan.id, quantity: 1, price: 400.00, paid: false, activity_id: france_hotel.id)
 
@@ -63,7 +63,7 @@ skiing = Vacation.create!(name: "Skiing", location: "Aspen", start_date: "2019-1
   skiing_user_2 = skiing.vacation_users.create!(user_id: chi.id, role: 0)
   skiing_user_4 = skiing.vacation_users.create!(user_id: earl.id, role: 0)
 
-    aspen_hotel = skiing.activities.create!(price: 1_500.00, num_attendees: 3, description: "Moutain Lodge", name: "Aspen Hotel", no_of_days: 3, cutoff_date: "2019-12-16", type: 1, user_id: jalena.id)
+    aspen_hotel = skiing.activities.create!(price: 1_500.00, num_attendees: 3, description: "Moutain Lodge", name: "Aspen Hotel", no_of_days: 3, cutoff_date: "2019-12-16", type: FixedCost, user_id: jalena.id)
 
       jalena_aspen_hotel = UserActivity.create!(user_id: jalena.id, quantity: 1, price: 500.00, paid: false, activity_id: aspen_hotel.id)
 
