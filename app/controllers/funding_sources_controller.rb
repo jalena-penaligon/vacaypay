8e156990e5853ded09feee34c92e046b3d6e130d
Chi Tran <chidtran@gmail.com>
2019-05-27 15:53:53 -0600

    Adds FundingSourcesController and show method
    Co-authored-by: Earl Stephens <34906415+earl-stephens@users.noreply.github.com>

---
+++ app/controllers/funding_sources_controller.rb
@@ -0,0 +1,7 @@
+class FundingSourcesController < ApplicationController
+  def show
+    @user = current_user
+    @iav = DwollaFundingService.new
+    @iav_token = @iav.get_iav_token(current_user)[:token]
+  end
+end
