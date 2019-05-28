aba5e016d47155438975637244ccc210799bfde9
Chi Tran <chidtran@gmail.com>
2019-05-28 09:51:55 -0600

    Changes iav_token to a local

--- app/controllers/funding_sources_controller.rb
+++ app/controllers/funding_sources_controller.rb
@@ -3,5 +3,8 @@ class FundingSourcesController < ApplicationController
     @user = current_user
     @iav = DwollaFundingService.new
     @iav_token = @iav.get_iav_token(current_user)[:token]
+    render locals: {
+      iav_token:  @iav.get_iav_token(current_user)[:token]
+    }
   end
 end
