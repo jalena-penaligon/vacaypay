40326efba63d299b37541660c4fc3edfc04cc553
Ethan Grab <ethangrab@gmail.com>
2019-05-29 17:49:31 -0600

    fixed helper methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   helper_method :current_user,
-                :activity_owner?
+                :activity_owner?,
+                :vacation_owner?
 
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
@@ -8,6 +9,10 @@ class ApplicationController < ActionController::Base
 
   def activity_owner?
     Activity.find(params[:id]).user_id == current_user.id
+  end
+
+  def vacation_owner?
+    Vacation.find(params[:vacation_id]).host == current_user
   end
 
 
