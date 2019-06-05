91b511548af9435a434a8f5ad404cfcb0ca8cf25
Jalena Taylor <45905026+jalena-penaligon@users.noreply.github.com>
2019-06-05 10:54:24 -0600

    Removes space on multiple emails invited

--- app/controllers/owner/invitations_controller.rb
+++ app/controllers/owner/invitations_controller.rb
@@ -7,9 +7,9 @@ class Owner::InvitationsController < ApplicationController
   def create
     owner = current_user
     vacation = Vacation.find(params[:vacation_id])
-    emails = params[:email_invitations].split(',')
-    email_list = User.check_for_existing_users(owner, emails, vacation.id)
-    email_list.each do |email|
+    emails = params[:email_invitations].split(',').map { |email| email.gsub(" ", "") }
+    non_existing_users = User.check_for_existing_users(owner, emails, vacation.id)
+    non_existing_users.each do |email|
       VacayInviterMailer.invite(owner, email, vacation).deliver_now
     end
     flash[:notice] = 'Successfully sent invitations!'
