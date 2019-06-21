25a4d9cf3f3e37b52cdd3173aa0fd5c5b1929c82
Jalena Taylor <45905026+jalena-penaligon@users.noreply.github.com>
2019-06-21 15:25:24 -0600

    Begins map implementation
    Co-authored-by: Earl Stephens <34906415+earl-stephens@users.noreply.github.com>

---
+++ app/assets/javascripts/index.js
@@ -0,0 +1,35 @@
+function initMap() {
+  var center = {lat: 41.8781, lng: -87.6298};
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 10,
+    center: center
+  });
+  var marker = new google.maps.Marker({
+    position: center,
+    map: map
+  });
+}
+
+var locations = [
+    ['Los Angeles', 34.052235, -118.243683],
+    ['Santa Monica', 34.024212, -118.496475],
+    ['Redondo Beach', 33.849182, -118.388405],
+    ['Newport Beach', 33.628342, -117.927933],
+    ['Long Beach', 33.770050, -118.193739]
+  ];
+...
+var infowindow =  new google.maps.InfoWindow({});
+var marker, count;
+for (count = 0; count < locations.length; count++) {
+    marker = new google.maps.Marker({
+      position: new google.maps.LatLng(locations[count][1], locations[count][2]),
+      map: map,
+      title: locations[count][0]
+    });
+google.maps.event.addListener(marker, 'click', (function (marker, count) {
+      return function () {
+        infowindow.setContent(locations[count][0]);
+        infowindow.open(map, marker);
+      }
+    })(marker, count));
+  }
