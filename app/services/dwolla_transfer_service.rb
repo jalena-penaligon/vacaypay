367390935794c595a7f948049cb084f25a64962c
Ethan Grab <ethangrab@gmail.com>
2019-06-27 10:45:57 -0600

    Added transfer summary email on transfer

--- app/services/dwolla_transfer_service.rb
+++ app/services/dwolla_transfer_service.rb
@@ -1,6 +1,7 @@
 class DwollaTransferService
-  def initialize(source, destination, activity, token)
+  def initialize(source, destination, user_activity, token)
     @token = token
+    @user_activity = user_activity
     @request_body =
     { :_links => {
            :source => {
@@ -12,11 +13,15 @@ class DwollaTransferService
        },
        :amount => {
            :currency => "USD",
-           :value => activity.price.to_f.to_s }
+           :value => user_activity.price.to_f.to_s }
     }
   end
 
   def create_transfer
+    destination_user = Activity.find(@user_activity.activity_id).owner
     transfer = @token.post "transfers", @request_body
+    if transfer.response_status == 201
+      DwollaTransferMailer.transfer(@user_activity.user_name, destination_user, @request_body).deliver_now
+    end
   end
 end
